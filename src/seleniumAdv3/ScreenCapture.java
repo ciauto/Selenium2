8ddcdfd8ec60d5307b3ab3fde7b57dba55f775b5
ciauto <naresh.busyqa@gmail.com>
2021-03-28 08:39:34 -0400

    added new files

--- src/seleniumAdv3/ScreenCapture.java
+++ src/seleniumAdv3/ScreenCapture.java
@@ -2,7 +2,6 @@ package seleniumAdv3;
 
 import java.io.File;
 import java.io.IOException;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
